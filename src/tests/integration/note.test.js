05717defbb67fc8da02fa71bd2770ffcaf54e101
Christopher Owen <s4115241@student.rmit.edu.au>
2024-08-20 23:58:46 +0700

    note.test.js update

--- src/tests/integration/note.test.js
+++ src/tests/integration/note.test.js
@@ -3,19 +3,29 @@ const supertest = require('supertest')
 const req = supertest(app)
 const Note = require("../../models/note").default
 
-// Integration Tests programmatically test the API used by the application
-
 describe('Add Note', () => {
+    beforeEach(async () => {
+        // Clear notes before each test to ensure isolation
+        await Note.deleteMany({})
+    })
+
     it('Should add a new note and redirect to login', async () => {
+        const newNote = {
+            title: "[INTEGRATION TEST] New Note",
+            description: "This note was created at " + new Date(),
+        }
 
         const res = await req
             .post('/')
-            .send({
-                title: "[INTEGRATION TEST] New Note",
-                description: "This note was created at " + new Date(),
-            })
+            .send(newNote)
 
+        // Check redirect response
         expect(res.statusCode).toEqual(302)
         expect(res.headers['location']).toEqual('/')
+
+        // Verify note has been added to the database
+        const notes = await Note.find({ title: newNote.title })
+        expect(notes.length).toBe(1)
+        expect(notes[0].description).toBe(newNote.description)
     })
 })
